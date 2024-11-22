da9d76e1e059ec8f6b301ed70216c38df4146c73
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2024-11-22 21:56:12 +0100

    benches: clean things up a lil

--- benches/kernels.rs
+++ benches/kernels.rs
@@ -2,7 +2,12 @@ extern crate criterion;
 extern crate uhyvelib;
 
 use std::{
-	array, env, f64::MIN, fs, io::Write, path::{Path, PathBuf}, process::{Command, Stdio}
+	array, env,
+	f64::MIN,
+	fs,
+	io::Write,
+	path::{Path, PathBuf},
+	process::{Command, Stdio},
 };
 
 use criterion::{criterion_group, Criterion};
@@ -54,57 +59,31 @@ fn is_program_in_path(program: &str) -> bool {
 
 // https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8/42186553
 unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    ::core::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        ::core::mem::size_of::<T>(),
-    )
+	::core::slice::from_raw_parts((p as *const T) as *const u8, ::core::mem::size_of::<T>())
 }
 
 pub fn run_open_test(c: &mut Criterion) {
-	use uhyve_interface::parameters::OpenParams;
-	
-	use uhyvelib::mem::MmapMemory;
-	use uhyvelib::MIN_PHYSMEM_SIZE;
-	use uhyve_interface::{GuestPhysAddr, GuestVirtAddr};
-	use uhyvelib::initialize_pagetables;
-	use std::ffi::{OsString, CString};
-	use std::os::unix::ffi::OsStrExt;
-	use log::error;
+	use std::ffi::CString;
 
-	let mut mem: MmapMemory = MmapMemory::new(
-		0,
-		MIN_PHYSMEM_SIZE * 2,
-		GuestPhysAddr::new(0),
-		false,
-		true
-	);
+	use uhyve_interface::{parameters::OpenParams, GuestPhysAddr};
+	use uhyvelib::{initialize_pagetables, mem::MmapMemory, MIN_PHYSMEM_SIZE};
+
+	const ARRAY_LEN: usize = 100;
+	const NAME_LEN: usize = 13;
+
+	let mem: MmapMemory =
+		MmapMemory::new(0, MIN_PHYSMEM_SIZE * 2, GuestPhysAddr::new(0), false, true);
 	// First MIN_PHYSMEM_SIZE is allocated, mem is presumed to be zero.
 	let mem_slice_mut = unsafe { mem.as_slice_mut() };
 	initialize_pagetables(mem_slice_mut.try_into().unwrap());
 
-	/*
-	unsafe {
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x00] = name[0];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x01] = name[1];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x02] = name[2];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x03] = name[3];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x04] = name[4];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x05] = name[5];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x06] = name[6];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x07] = name[7];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x08] = name[8];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x09] = name[9];
-		mem.as_slice_mut()[MIN_PHYSMEM_SIZE + 0x0a] = name[10];
-	}*/
-
-	const ARRAY_LEN: usize = 100;
-	const NAME_LEN: usize = 13;
 	// Example: "/tmp/012.txt"
 	let names: Vec<CString> = (0..ARRAY_LEN)
 		.map(|i| {
 			let string: String = format!("{}{}{}", "/tmp/", format!("{:0>3}", i), ".txt");
 			CString::new(string.as_bytes()).unwrap()
-		}).collect();
+		})
+		.collect();
 	assert_eq!(names[0].as_bytes_with_nul().len(), NAME_LEN);
 	assert_eq!(names.len(), ARRAY_LEN);
 
@@ -114,52 +93,29 @@ pub fn run_open_test(c: &mut Criterion) {
 	for i in 0..ARRAY_LEN {
 		let name = names[i].as_bytes_with_nul();
 		for j in 0..NAME_LEN {
-			mem_slice_mut[offset + i*NAME_LEN + j] = name[j];
+			mem_slice_mut[offset + i * NAME_LEN + j] = name[j];
 		}
 	}
 
 	if false {
 		let mut slice = mem_slice_mut.to_vec();
 		for i in 0..slice.len() {
-			if (slice[i] == 0x2f && slice[i+1] == 0x74) {
-				panic!("it works! {}, {}", i, i+1);
+			if (slice[i] == 0x2f && slice[i + 1] == 0x74) {
+				panic!("it works! {}, {}", i, i + 1);
 			}
 		}
 	}
 
-	uhyvelib::hypercall::open(&mem, &mut OpenParams {
-		name: GuestPhysAddr::new(MIN_PHYSMEM_SIZE as u64 + 13),
-		mode: 0x777,
-		flags: 0o100 | 0o2,
-		ret: 0,
-	});
+	uhyvelib::hypercall::open(
+		&mem,
+		&mut OpenParams {
+			name: GuestPhysAddr::new((MIN_PHYSMEM_SIZE + NAME_LEN) as u64),
+			mode: 0x777,
+			flags: 0o100 | 0o2,
+			ret: 0,
+		},
+	);
 
-	panic!("Yay!")
-
-	/*
-	let firstopenparam: &mut OpenParams = &mut OpenParams {
-		name: GuestPhysAddr::new(unsafe { mem.host_address.add((MIN_PHYSMEM_SIZE as u8) as usize)} as u64),
-		flags: 0o100 | 0o2,
-		mode: 0x777,
-		ret: 0
-	};
-	uhyvelib::hypercall::open(&mem, firstopenparam);
-	let retcode = firstopenparam.ret;
-	*/
-
-	/*
-	let first_openparam = unsafe {any_as_u8_slice(openparams_vec.into_iter().next().unwrap())}.to_vec();
-	let size_first_openparam: usize = first_openparam.len() * size_of::<u8>();
-	let second_openparam = unsafe {any_as_u8_slice(openparams_vec.into_iter().next().unwrap())}.to_vec();
-	let size_second_openparam: usize = second_openparam.len() * size_of::<u8>();
-	env_logger::builder().is_test(true).try_init().unwrap();
-	error!("{}", size_first_openparam);
-	error!("{}", size_second_openparam);
-	error!("{}", std::mem::size_of::<OpenParams>());
-	let mut slice = unsafe { mem.slice_at_mut(GuestPhysAddr::new(MIN_PHYSMEM_SIZE as u64), size_first_openparam) }.unwrap();
-	slice.write(&first_openparam).unwrap();
-	uhyvelib::hypercall::open(&mem, openparam);
-	*/
 	/*
 	let mut group = c.benchmark_group("compile_hello_world");
 	group.sample_size(100);
@@ -189,7 +145,10 @@ pub fn run_compile_hello_world(c: &mut Criterion) {
 
 	// Unlike in complete_binary.rs, this dark magic is necessary because of ownership. (???)
 	let hello_world_pathbuf = build_hermit_bin("hello_world");
-	assert!(hello_world_pathbuf.exists(), "compiled hello_world kernel missing");
+	assert!(
+		hello_world_pathbuf.exists(),
+		"compiled hello_world kernel missing"
+	);
 	let hello_world_path = hello_world_pathbuf.into_os_string().into_string().unwrap();
 
 	let mut group = c.benchmark_group("compile_hello_world");
@@ -223,27 +182,27 @@ pub fn run_file_test(c: &mut Criterion) {
 
 	// Unlike in complete_binary.rs, this dark magic is necessary because of ownership. (???)
 	let file_test_pathbuf = build_hermit_bin("file_test");
-	assert!(file_test_pathbuf.exists(), "compiled file_test kernel missing");
+	assert!(
+		file_test_pathbuf.exists(),
+		"compiled file_test kernel missing"
+	);
 	let file_test_path = file_test_pathbuf.into_os_string().into_string().unwrap();
 
 	let mut group = c.benchmark_group("run_file_test");
 	group.sample_size(100);
 
-	group.bench_function(
-		"uhyve file_test",
-		|b| {
-			b.iter(|| {
-				let status = Command::new(&uhyve_path)
-					.arg(&file_test_path)
-					.arg("-m")
-					.arg("64MiB")
-					.stdout(Stdio::null())
-					.status()
-					.expect("failed to execute process");
-				assert!(status.success());
-			})
-		},
-	);
+	group.bench_function("uhyve file_test", |b| {
+		b.iter(|| {
+			let status = Command::new(&uhyve_path)
+				.arg(&file_test_path)
+				.arg("-m")
+				.arg("64MiB")
+				.stdout(Stdio::null())
+				.status()
+				.expect("failed to execute process");
+			assert!(status.success());
+		})
+	});
 }
 
 // criterion_group!(run_kernel_group, run_compile_hello_world, run_file_test);
